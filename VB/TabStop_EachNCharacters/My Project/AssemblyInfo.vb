d88966454cc4cd8c3d938d2f7777ff73a4855007
dxMaria <mariya.nikulina@devexpress.com>
2023-01-26 20:55:42 +0400

    Update to NET 6

--- VB/TabStop_EachNCharacters/My Project/AssemblyInfo.vb
+++ VB/TabStop_EachNCharacters/My Project/AssemblyInfo.vb
@@ -8,14 +8,7 @@ Imports System.Windows
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("TabStop_EachNCharacters")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("TabStop_EachNCharacters")>
-<Assembly: AssemblyCopyright("Copyright ©  2014")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
+
 
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
@@ -49,5 +42,4 @@ Imports System.Windows
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+
