426b58c046ceed5c106ba4a6a13d5664bf5568d5
DevExpressExampleBot <fakeEmail>
2023-01-26 21:02:07 +0400

    Source auto update [skip ci]

---
+++ VB/TabStop_EachNCharacters/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace TabStop_EachNCharacters
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
